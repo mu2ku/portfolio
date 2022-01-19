5b00936ef69412468edb7947e47bec752b2de8b0
mu2ku <gmutuku@gmail.com>
2022-01-18 23:23:07 -0500

    commit 2 additional portfolio pieces to portfolio page

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -5,13 +5,14 @@ const Works = () => {
   return (  
     <>
     <main>
+      {/* Dominos */}
       <div className={styles.works1_container}>
         <div>
           <img className={styles.works1_image} src="images/dominos.jpg" alt="dominos" /> 
         </div>
         <div className={styles.works1_description}>
           <span className={styles.works1_title}>DOMINOS</span>
-          <p>A web browser-based game of dominoes versus a computer.</p>
+          <p>A web browser-based game of dominos versus a computer.</p>
           <i><p>HTML	&#9679; CSS 	&#9679; JavaScript</p></i>
           <a href="https://sei-dominos.surge.sh/" rel="noreferrer" target="_blank">VIEW LIVE</a> | <a href="https://github.com/mu2ku/Dominos" rel="noreferrer" target="_blank">VIEW REPO</a>
           <div className={styles.works1_counter_container}>
@@ -19,6 +20,7 @@ const Works = () => {
           </div>
         </div>
       </div>
+      {/* Lingua */}
       <div className={styles.works2_container}>
         <div className={styles.works1_description}>
           <span className={styles.works1_title}>LINGUA</span>
@@ -33,7 +35,36 @@ const Works = () => {
           <img className={styles.works1_image} src="images/lingua.png" alt="lingua" /> 
         </div>
       </div>
-      
+      {/* Genshin Builds */}
+      <div className={styles.works1_container}>
+        <div>
+          <img className={styles.works1_image} src="images/genshin.png" alt="genshin" /> 
+        </div>
+        <div className={styles.works1_description}>
+          <span className={styles.works1_title}>GENSHIN BUILDS</span>
+          <p>Genshin Builds is an application where players of the popular action RPG, Genshin Impact, can create, mix and match potential team builds!</p>
+          <i><p>HTML	&#9679; CSS 	&#9679; JavaScript &#9679; MongoDB &#9679; Express &#9679; React &#9679; Node.js &#9679; REST API</p></i>
+          <a href="https://genshin-team-builder.herokuapp.com/" rel="noreferrer" target="_blank">VIEW LIVE</a> | <a href="https://github.com/mu2ku/genshin-builds" rel="noreferrer" target="_blank">VIEW REPO</a>
+          <div className={styles.works1_counter_container}>
+            <span className={styles.works1_counter}>03</span>
+          </div>
+        </div>
+      </div>
+      {/* J'ai Faim, Je Mange */}
+      <div className={styles.works2_container}>
+        <div className={styles.works1_description}>
+          <span className={styles.works1_title}>J'AI FAIM, JE MANGE</span>
+          <p className={styles.works_shortDes}>"J'ai Faim, Je Mange" is a Python/Django web application for users to search, collect, and add their favorite recipes.</p>
+          <i><p>HTML	&#9679; CSS 	&#9679; JavaScript &#9679; Python &#9679; Django &#9679; REST API</p></i>
+          <a href="https://jai-104.herokuapp.com/" rel="noreferrer" target="_blank">VIEW LIVE</a> | <a href="https://github.com/mu2ku/J-ai-Faim-Je-Mange" rel="noreferrer" target="_blank">VIEW REPO</a>
+          <div className={styles.works2_counter_container}>
+            <span className={styles.works2_counter}>04</span>
+          </div>
+        </div>
+        <div>
+          <img className={styles.works1_image} src="images/jai.png" alt="jai" /> 
+        </div>
+      </div>
     </main>
     </>
   );
